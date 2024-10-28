25de3611f19262bd0bfc14010461b139c4bcbd72
Admane Mohamed <mohamed.admane@etu.univ-orleans.fr>
2024-10-28 18:51:13 +0100

    question5

--- collec_management/views.py
+++ collec_management/views.py
@@ -4,3 +4,8 @@ from .models import Collec
 # Create your views here.
 def about(request):
 	return HttpResponse("about page.")
+
+def collection(request, id):
+	c = get_object_or_404(Collec, id=id)
+
+	return render(request, "collection.html", {"c": c})
